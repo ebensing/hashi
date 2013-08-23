6f58b58ca0b2e6e844eb2c812a98287766f01849
EJ Bensing <ebensing@rm-dash-r.com>
2013-08-23 14:30:25 -0700

    added comments to app.js

--- app.js
+++ app.js
@@ -14,10 +14,16 @@ var Task = AsanaModels.Task;
 var Issue = GithubModels.Issue;
 var Hook = GithubModels.Hook;
 
+// setup the authentication stuff
 AsanaConnector.setKey(config.asanaKey);
 
 GithubConnector.setAuthInfo(config.githubInfo.userName,
     config.githubInfo.password);
+
+/**
+ * This is the main startup method. It will also run periodically to make sure
+ * everything is in sync.
+ */
 
 function main() {
 
@@ -72,9 +78,19 @@ function main() {
   getAllIssues(repos, cont);
 }
 
-
-// once we get here, all of the data has been collected and is saved in the
-// database. Time to process it and update anything that needs it
+/**
+ * Once we get here, all of the data has been collected and is saved in the
+ * database. Time to process it and update anything that needs it
+ *
+ * @param {Array} repos - This is an array of the repos that need to be watched
+ *                        and processed. They have additional fields on them
+ *                        necessary for the processing, see main().
+ * @param {Object} workMap - This is an object of workspace names that maps to
+ *                           workspace Ids
+ * @param {Object} projectMap - This is an object of project names that maps to
+ *                              project Ids
+ *
+ */
 function parse(repos, workMap, projectMap) {
   console.log("All Data collected!");
 
@@ -109,6 +125,15 @@ function parse(repos, workMap, projectMap) {
   });
 }
 
+/**
+ * This function checks to make sure the issue hook exists on the repo, and if
+ * it does not, creates it.
+ *
+ * @param {Object} repo - This is the repo object that describes the repo we
+ *                        want to check
+ *
+ */
+
 function checkHooks(repo) {
   var cond = { repo : { name : repo.name, user : repo.user } };
   Hook.findOne(cond, function (err, hook) {
@@ -134,6 +159,15 @@ function checkHooks(repo) {
   });
 }
 
+/**
+ * This decides whether a Task exists in Asana for this issue. If it does, then
+ * sync it, else create a new task.
+ *
+ * @param {Issue} issue - This is the issue to check for
+ * @param {Function} callback
+ *
+ */
+
 function processIssue(issue, callback) {
 
   var tag = '"(gh ' + issue.number.toString() + ')"';
@@ -152,6 +186,15 @@ function processIssue(issue, callback) {
     }
   });
 }
+
+/**
+ * Syncs an existing Issue and an existing task
+ *
+ * @param {Issue} issue
+ * @param {Task} task
+ * @param {Function} callback - function(err)
+ *
+ */
 
 function syncIssue(issue, task, callback) {
   var changeSet = {};
@@ -183,6 +226,16 @@ function syncIssue(issue, task, callback) {
   }
 }
 
+/**
+ * Create a new Asana Task for the specified Github Issue
+ *
+ * @param {Issue} issue
+ * @param {String} tag - This is the tag that goes in the Task notes field to
+ *                       associate it with the Github Issue
+ * @param {Function} callback - function(err, task)
+ *
+ */
+
 function createAsanaTask(issue, tag, callback) {
 
   var task = new Task({
@@ -203,6 +256,20 @@ function createAsanaTask(issue, tag, callback) {
   });
 }
 
+/**
+ *
+ * Fetches all of the issues for a given repository that belong to a given user
+ * (specified on the repo object). After this call has returned, the MongoDB
+ * data will be up to date.
+ *
+ * @param {Array} repos - repos to get issues for. Each item in the array must
+ *                        contain data on the repo owner name and repo name, as
+ *                        well as, information on the user whose issues you
+ *                        want to get
+ * @param {Function} callback - function(err, issues)
+ *
+ */
+
 function getAllIssues(repos, callback) {
   async.each(repos, function (repo, cb) {
     GithubConnector.getAllRepoIssues(repo, repo.assignee, function (err, issues) {
@@ -214,12 +281,29 @@ function getAllIssues(repos, callback) {
   }, callback);
 }
 
+/**
+ * Takes an array of items and a reference to a mongoose model, and then
+ * upserts them into MongoDB based off of the `id` field.
+ *
+ * @param {Array} items - array of plain objects to upsert
+ * @param {Model} iClass - the model that the objects conform to
+ * @param {Function} callback - function(err, savedModels)
+ *
+ */
+
 function saveItems(items, iClass, callback) {
   async.map(items, function (item, cb) {
     var raw = item.toObject();
     iClass.findOneAndUpdate({ id : item.id }, raw, { upsert : true }, cb);
   }, callback);
 }
+
+/**
+ * Gets all workspaces for the user associated with the Asana API Key provided.
+ *
+ * @param {Function} callback - function(err, workspaces)
+ *
+ */
 
 function getAllWorkspaces(callback) {
   AsanaConnector.getWorkspaces(function (err, workspaces) {
@@ -229,6 +313,15 @@ function getAllWorkspaces(callback) {
     saveItems(workspaces, Workspace, callback);
   });
 }
+
+/**
+ * Based off the config file, grabs the Asana Projects that we are interested
+ * in adding things to
+ *
+ * @param {Array} workspaces - this is an array of workspace models
+ * @param {Function} callback - function(err, projects)
+ *
+ */
 
 function getMonitoredProjects(workspaces, callback) {
 
@@ -258,6 +351,14 @@ function getMonitoredProjects(workspaces, callback) {
   });
 }
 
+/**
+ * Gets the tasks we care about based off of the projects list.
+ *
+ * @param {Array} projects - array of projects for which to get the tasks for
+ * @param {Function} callback - function(err, tasks)
+ *
+ */
+
 function getMonitoredTasks(projects, callback) {
 
   var monitoredProjects = config.bindings.map(function (item) {
@@ -285,14 +386,29 @@ function getMonitoredTasks(projects, callback) {
   });
 }
 
+/**
+ * This is what gets called anytime there is an error. Should probably be doing
+ * more graceful failure here.
+ *
+ * @param {Error} err
+ *
+ */
+
 function onError(err) {
   console.log(err);
   mongoose.disconnect();
 }
-mongoose.connect("mongodb://localhost/taskSync", function (err) {
+
+/**
+ * Connect to the database
+ */
+
+mongoose.connect("mongodb://localhost/hashi", function (err) {
   if (err) throw err;
+  // run the main loop and sync up the data
   main();
 
+  // create the server that will be watching for the webhooks
   http.createServer(function (req, res) {
     var data = "";
     req.on('data', function (d) {
